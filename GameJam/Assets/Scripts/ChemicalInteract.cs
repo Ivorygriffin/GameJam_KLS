c7374e5ba52269bdfa6e550b4d3c3eb3596e56cb
AIE\s210559 <s210559@students.aie.edu.au>
2021-09-20 13:59:39 +0930

    sscript test scene 2

--- GameJam/Assets/Scripts/ChemicalInteract.cs
+++ GameJam/Assets/Scripts/ChemicalInteract.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ChemicalInteract : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public UnityEvent Key1; 
+    public UnityEvent Key2;
+    public UnityEvent Key3;
+    public UnityEvent Key4;
+    public UnityEvent Key5;
+    public UnityEvent Key6;
+
+    [SerializeField] private UnityEvent hii;
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            Key1.Invoke();
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            Key2.Invoke();
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            Key3.Invoke();
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            Key4.Invoke();
+        } 
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            Key5.Invoke();
+        } 
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            Key6.Invoke();
+        }
     }
 }
